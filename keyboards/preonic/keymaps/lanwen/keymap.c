75db9ee5b55124c58db57b74966cfe657d2a5a9c
Kirill Merkushev <lanwen@yandex.ru>
2020-11-04 11:48:47 +0100

    cmd c, cmd v

--- keyboards/preonic/keymaps/lanwen/keymap.c
+++ keyboards/preonic/keymaps/lanwen/keymap.c
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+--------------+-------+------+--------------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  | Left | Down |  Up          | Right | End  |  |           |
  * |------+------+------+------+------+------|------+------+--------------+-------+------+--------------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO |         | Home  | End  |              |
+ * |      |  F7  |  F8  |CMD+C |CMD+V |  F11 |  F12 |ISO ~ |ISO |         | Home  | End  |              |
  * |------+------+------+------+------+------+------+------+--------------+-------+------+--------------|
  * |      |      |      |      |      |             |      | Brightness - | Vol-  | Vol+ | Brightness + |
  * `----------------------------------------------------------------------------------------------------'
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,   KC_MINS, KC_EQL,  KC_BSPC,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,   KC_LPRN, KC_RPRN,  KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN,    KC_UP,     KC_RGHT, KC_END,   KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS),KC_HOME, KC_END,   _______,
+  _______, KC_F7,   KC_F8,   RGUI(KC_C),   RGUI(KC_V),  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS),KC_HOME, KC_END,   _______,
   _______, _______, _______, _______, _______, _______, _______, _______,    KC_BRMD,   KC_VOLD, KC_VOLU,  KC_BRMU
 ),
 
